2e22e99aa7b6325969b4f62c285022699c7ef7b3
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-12 10:42:38 +0100

    el ejercicio 1 terminado

--- src/ejercicios4_1Recorrido/Ejercicio1.java
+++ src/ejercicios4_1Recorrido/Ejercicio1.java
@@ -3,13 +3,26 @@ package ejercicios4_1Recorrido;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		
+		/*Realiza un procedimiento que cree un array de enteros y muestre por cada 
+		 *posición si el elemento que contiene es par o impar. 
+		 */
 		new Ejercicio1().show();
 
 	}
 	
 	public void show() {
+		int[] elements= {5, 8, 6, 7, 1};
 		
+		for (int i=0; i< elements.length; i++) {
+			
+			if(elements[i] %2 == 0) {
+				System.out.printf("El elemento de la posición %d es par\n", i);
+				
+			}else {
+				System.out.printf("El elemento de la posición %d es impar\n", i);
+
+			}
+		}
 	}
 
 }
