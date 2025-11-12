49195ebf10812f0d7ba5061428ad04c89d926dc8
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-12 11:03:32 +0100

    ejercicio 2 terminado

--- src/ejercicios4_1Recorrido/Ejercicio2.java
+++ src/ejercicios4_1Recorrido/Ejercicio2.java
@@ -1,14 +1,42 @@
 package ejercicios4_1Recorrido;
+import java.util.*;
 
 public class Ejercicio2 {
 
 public static void main(String[] args) {
-		
+		/*Realiza un procedimiento que cree un array de enteros y le solicite un 
+		 *booleano al usuario. Si el booleano es true, el procedimiento debe mostrar
+		 *los elementos que ocupan las posiciones pares. Si el booleano es false, 
+		 *el procedimiento debe mostrar los elementos que ocupan las posiciones impares.
+		 */
 		new Ejercicio2().show();
 
 	}
 	
 	public void show() {
+		@SuppressWarnings("resource")
+		Scanner kb= new Scanner(System.in);
+		int[] elements= {21, 45, 7, 3, 52, 61};
+		boolean posicion;
+		
+		System.out.printf("Escribe \"true\" si quieres las posiciones pares o \"false\" si quieres las impares\n");
+		posicion= kb.nextBoolean();
+		
+		for(int i=0; i<elements.length; i++) {
+			
+			if(posicion) {
+				if(i%2==0) {
+					System.out.printf("Posicion %d: %d\n", i, elements[i]);
+					
+				}
+				
+			}else {
+				if(i%2!=0) {
+					System.out.printf("Posicion %d: %d\n", i, elements[i]);
+				}
+				
+			}
+		}
 		
 	}
 
