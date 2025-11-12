37ef3bb0e8a0e7e04c36ea76542b54db677c7954
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-12 10:52:15 +0100

    comentarios

--- src/ejercicios4_1Recorrido/Ejercicio1.java
+++ src/ejercicios4_1Recorrido/Ejercicio1.java
@@ -13,9 +13,9 @@ public class Ejercicio1 {
 	public void show() {
 		int[] elements= {5, 8, 6, 7, 1};
 		
-		for (int i=0; i< elements.length; i++) {
+		for (int i=0; i< elements.length; i++) { //recorre el array hasta el final
 			
-			if(elements[i] %2 == 0) {
+			if(elements[i] %2 == 0) { //comprueba si el elemento en esa posicion es par
 				System.out.printf("El elemento de la posición %d es par\n", i);
 				
 			}else {
