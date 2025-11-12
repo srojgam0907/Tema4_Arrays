66b0648b639e443f780f5c42304ef1809aa9579d
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-12 09:19:43 +0100

    creacion de la función show

--- src/ejercicios4_1Recorrido/Ejercicio1.java
+++ src/ejercicios4_1Recorrido/Ejercicio1.java
@@ -3,8 +3,13 @@ package ejercicios4_1Recorrido;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		new Ejercicio1().show();
 
+	}
+	
+	public void show() {
+		
 	}
 
 }
--- src/ejercicios4_1Recorrido/Ejercicio2.java
+++ src/ejercicios4_1Recorrido/Ejercicio2.java
@@ -2,9 +2,14 @@ package ejercicios4_1Recorrido;
 
 public class Ejercicio2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
+		
+		new Ejercicio2().show();
 
+	}
+	
+	public void show() {
+		
 	}
 
 }
