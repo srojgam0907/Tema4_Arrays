1f02aeaba07aa11158a03b1234802e2b4077b9be
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-15 19:28:14 +0100

    Ejercicio 1 empezado

--- src/ejercicios4_1ArraysParametros/Ejercicio1.java
+++ src/ejercicios4_1ArraysParametros/Ejercicio1.java
@@ -3,8 +3,16 @@ package ejercicios4_1ArraysParametros;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Realiza una función que reciba un varargs de enteros 
+		 *y devuelva la multiplicación de todos los números.
+		 */
+		
+		new Ejercicio1().show();
 
+	}
+	
+	public void show() {
+		
 	}
 
 }
