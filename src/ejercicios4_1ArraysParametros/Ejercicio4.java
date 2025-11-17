db0dd184b16c50e92fb56733783afce379d0b99f
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-17 09:31:19 +0100

    Ejercicio 4 empezado

--- src/ejercicios4_1ArraysParametros/Ejercicio4.java
+++ src/ejercicios4_1ArraysParametros/Ejercicio4.java
@@ -3,8 +3,17 @@ package ejercicios4_1ArraysParametros;
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Realiza un procedimiento que reciba un array de enteros y un número. 
+		 * El procedimiento tendrá que mostrar: 
+		 * 		-En qué posiciones del array aparece dicho número.
+		 * 		-Cuántas veces aparece dicho número en total en el array. 
+		 */
 
+		new Ejercicio4().show();
+		
 	}
 
+	public void show() {
+		
+	}
 }
