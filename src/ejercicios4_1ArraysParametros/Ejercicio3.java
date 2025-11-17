73cc66d83a33d59e863c78991e9c199aae52fb9a
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-17 08:31:00 +0100

    Ejercicio 3 empezado

--- src/ejercicios4_1ArraysParametros/Ejercicio3.java
+++ src/ejercicios4_1ArraysParametros/Ejercicio3.java
@@ -3,8 +3,16 @@ package ejercicios4_1ArraysParametros;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*Realiza una función que reciba un array de enteros 
+		 * y devuelva la suma de todos los elementos.
+		 */
+		
+		new Ejercicio3().show();
+	
+	}
+	
+	public void show() {
+		
 	}
 
 }
