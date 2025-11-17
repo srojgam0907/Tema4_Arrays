6d4569f9b312cc950af9ad72247973bccd336016
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-17 08:49:01 +0100

    Ejercicio 2 casi termindo (no funciona)

--- src/ejercicios4_1ArraysParametros/Ejercicio2.java
+++ src/ejercicios4_1ArraysParametros/Ejercicio2.java
@@ -14,6 +14,26 @@ public class Ejercicio2 {
 	}
 	
 	public void show() {
+		int[] numbers= {2, 5, 8};
+		
+		System.out.println("Array sin cambiar: ");
+		numbers.toString();
+		
+		modificarArray(numbers);
+		
+		
+	}
+	
+	public void modificarArray(int[] numbers) {
+		for(int i=0; i< numbers.length; i++) {
+			if(numbers[i] %2 != 0){
+				numbers[i]++;
+				
+			}
+		}
+		
+		System.out.println("Array cambiado: ");
+		numbers.toString();
 		
 	}
 
