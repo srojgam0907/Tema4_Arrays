063981e5cf75bdee4d1c0aacda2f00e1ed9d0af7
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-18 09:54:15 +0100

    Creacion del boletin 3 de los ejercicios 4.1

---
+++ src/ejercicios4_1RetornoArrays/Ejercicio2.java
@@ -0,0 +1,10 @@
+package ejercicios4_1RetornoArrays;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ejercicios4_1RetornoArrays/Ejercicio5.java
@@ -0,0 +1,10 @@
+package ejercicios4_1RetornoArrays;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
