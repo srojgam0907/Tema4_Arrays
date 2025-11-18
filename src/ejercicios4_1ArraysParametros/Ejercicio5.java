d886d4d4e05cb8433e4c40eb2c7a83eb8e6869dc
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-18 09:04:23 +0100

    Ejercicio 5 empezado

--- src/ejercicios4_1ArraysParametros/Ejercicio5.java
+++ src/ejercicios4_1ArraysParametros/Ejercicio5.java
@@ -3,8 +3,16 @@ package ejercicios4_1ArraysParametros;
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Realiza un procedimiento que reciba un array de números 
+		 *comprendidos entre 0 y 9 y muestre un histograma
+		 */
+		
+		new Ejercicio5().show();
 
+	}
+	
+	public void show() {
+		
 	}
 
 }
