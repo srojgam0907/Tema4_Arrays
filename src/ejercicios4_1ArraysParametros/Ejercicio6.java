f81cde877daaf1c37701e271b0adb37847d2a347
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-18 09:07:22 +0100

    Ejercicio 6 empezado

--- src/ejercicios4_1ArraysParametros/Ejercicio6.java
+++ src/ejercicios4_1ArraysParametros/Ejercicio6.java
@@ -3,8 +3,16 @@ package ejercicios4_1ArraysParametros;
 public class Ejercicio6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Realiza una función recursiva que reciba un array 
+		 *de enteros y devuelva un booleano para indicar  
+		 *si el array es capicúa.
+		 */
+		
+		new Ejercicio6().show();
 
 	}
 
+	public void show() {
+		
+	}
 }
