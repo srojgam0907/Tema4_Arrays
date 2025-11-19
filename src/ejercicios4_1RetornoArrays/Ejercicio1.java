08cdfea9ca6f5dc3ee8217656f2b2f5140335b3c
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-19 10:46:49 +0100

    Ejercicio 1 empezado

--- src/ejercicios4_1RetornoArrays/Ejercicio1.java
+++ src/ejercicios4_1RetornoArrays/Ejercicio1.java
@@ -3,8 +3,20 @@ package ejercicios4_1RetornoArrays;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Realiza una función que reciba un array de caracteres, un carácter y 
+		 * una posición. La función tiene que insertar el carácter en la posición 
+		 * indicada, desplazando el resto de caracteres una posición. El array debe 
+		 * tener la última posición libre para no perder información. No utilizar 
+		 * un array auxiliar, sino que el desplazamiento se tiene que hacer sobre el 
+		 * mismo array. La función tiene que devolver el array con el carácter insertado.
+		 */
+		
+		new Ejercicio1().show();
 
+	}
+	
+	public void show() {
+		
 	}
 
 }
