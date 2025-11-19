0fd55b8e48f903970c91e67539ef19e36a37835f
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-19 11:08:02 +0100

    Ejercicio 5 empezado

--- src/ejercicios4_1RetornoArrays/Ejercicio5.java
+++ src/ejercicios4_1RetornoArrays/Ejercicio5.java
@@ -3,8 +3,18 @@ package ejercicios4_1RetornoArrays;
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Realiza una función recursiva que reciba un array de números 
+		 *decimales ordenados y un número decimal y busque el número 
+		 *decimal en el array. Si lo encuentra devuelve la posición, 
+		 *si no lo encuentra, devuelve un -1.
+		 */
+		
+		new Ejercicio5().show();
 
+	}
+	
+	public void show() {
+		
 	}
 
 }
