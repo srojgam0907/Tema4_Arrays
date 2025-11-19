492a3fa265e4b2535849be4c27b5f844653ffc19
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-19 10:42:36 +0100

    Ejercicio 5 correccion sin terminar

--- src/ejercicios4_1ArraysParametros/Ejercicio5.java
+++ src/ejercicios4_1ArraysParametros/Ejercicio5.java
@@ -12,15 +12,40 @@ public class Ejercicio5 {
 	}
 	
 	public void show() {
-		int[] numbers = null;
+		int[] numbers = new int[] {6, 4, 4, 1, 9, 7, 5, 6, 4, 2, 3, 9, 5, 6, 4};
 		
 		histograma(numbers);
 	}
 	
-	public void histograma(int[] numbers) {
-		numbers= new int[] {6, 4, 4, 1, 9, 7, 5, 6, 4, 2, 3, 9, 5, 6, 4};
+	public void histograma(int[] nums) { //Cambiar nombre a numbers 
+		int max=0;
+		int[] count= new int[10];
 		
-		for(int i=0; i<numbers.length; i++) {
+		for(int n : nums) {  
+			if(n >=0 && n <=9) {
+				count[n]++;
+			}
+		}
+		
+		for(int f : count) {
+			if(f > max) {
+				max= f;
+			}
+		}
+		
+		for(int nivel= max; nivel>0; nivel--) {
+			for(int i=0; i<count.length; i++) {
+				if(count[i] >= nivel) {
+					System.out.print("* ");
+					
+				}else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		
+		for(int i=0; i<10; i++) {
 			
 		}
 		
