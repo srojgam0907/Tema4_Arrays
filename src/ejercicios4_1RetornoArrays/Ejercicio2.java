f8c40cb4648c18631127a16093b5b939870d3447
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-19 10:49:43 +0100

    Ejercicio 2 empezado

--- src/ejercicios4_1RetornoArrays/Ejercicio2.java
+++ src/ejercicios4_1RetornoArrays/Ejercicio2.java
@@ -3,8 +3,16 @@ package ejercicios4_1RetornoArrays;
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Realiza una función que reciba un array y devuelva otro 
+		 *array con sus elementos invertidos.
+		 */
 
+		new Ejercicio2().show();
+		
+	}
+	
+	public void show() {
+		
 	}
 
 }
