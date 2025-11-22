540dbdb79f4dc8e3d8dc068872155db59f762dec
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-22 12:28:17 +0100

    Ejercicio 6 hecho en iterativo

--- src/ejercicios4_1ArraysParametros/Ejercicio6.java
+++ src/ejercicios4_1ArraysParametros/Ejercicio6.java
@@ -5,7 +5,7 @@ public class Ejercicio6 {
 	public static void main(String[] args) {
 		/*Realiza una función recursiva que reciba un array 
 		 *de enteros y devuelva un booleano para indicar  
-		 *si el array es capicúa.
+		 *si el array es capicúo.
 		 */
 		
 		new Ejercicio6().show();
@@ -14,7 +14,7 @@ public class Ejercicio6 {
 
 	public void show() {
 		int[] numCapicuo= {8,2,5,5,2,8};
-		int[] numNoCapicuo= {8,3,5,9,4,8};
+		//int[] numNoCapicuo= {8,3,5,9,4,8};
 		boolean capicuo= false;
 		
 		capicuo= esCapicuo(numCapicuo);
@@ -29,22 +29,26 @@ public class Ejercicio6 {
 		
 	}
 	
-	public boolean esCapicuo(int[] numCapicuo) {
-		
-		for(int i=0; i<numCapicuo.length/2; i++) {
-			if(numCapicuo[i]!=numCapicuo[numCapicuo.length-i-1]) {
-				return false;
-				
-			} else {
-				return true; 
-				
-			}
-			
-		}
+	public boolean esCapicuo(int[] num) { 
 		
 		
-		return true;
+		
+		
 	}
+	
+	
+	/* ITERATIVO
+	for(int i=0; i<num.length/2; i++) {
+		
+		if(num[i] != num[num.length-1-i]) {
+			return false; 
+			
+		} 
+		
+	}
+	
+	return true;
+	*/
 }
 
 
