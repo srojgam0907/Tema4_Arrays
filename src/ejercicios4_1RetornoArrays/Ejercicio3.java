b935cdaff738224b69d4f82769e58154f62818b6
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-24 10:58:08 +0100

    Ejercicio 3 cambio en funcion arrayOrder

--- src/ejercicios4_1RetornoArrays/Ejercicio3.java
+++ src/ejercicios4_1RetornoArrays/Ejercicio3.java
@@ -50,8 +50,8 @@ public class Ejercicio3 {
 		int value= 0;
 		
 		for(int i=0; i<array.length-1; i++) {
-			for(int j=0; j<array.length-1; j++) {
-				if(array[j] > array[j+1]) { //Lo ordena con el método burbuja
+			for(int j=0; j<array.length-1-i; j++) { //Para evitar comparaciones innecesarias
+				if(array[j] > array[j+1]) { //Lo ordena de menor a mayor con el método burbuja
 					value= array[j+1];
 					array[j+1]= array[j];
 					array[j]= value;
