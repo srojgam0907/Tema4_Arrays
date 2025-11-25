ebb109ce6adc6772270e1ca310ab0ffb7a7ffc10
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-25 09:30:11 +0100

    Ejercicio 2 empezado

--- src/ejercicios4_2ArraysMultidimensionales/Ejercicio2.java
+++ src/ejercicios4_2ArraysMultidimensionales/Ejercicio2.java
@@ -3,8 +3,16 @@ package ejercicios4_2ArraysMultidimensionales;
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Realiza una función que reciba un array de tres dimensiones 
+		 *de números decimales y devuelva cuál es el elemento mayor. 
+		 */
+		
+		new Ejercicio2().show();
 
+	}
+	
+	public void show() {
+		
 	}
 
 }
