9f35406d170aee6a96bc0e0591aefb55c316bb25
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-25 09:31:11 +0100

    Ejercicio 3 empezado

--- src/ejercicios4_2ArraysMultidimensionales/Ejercicio3.java
+++ src/ejercicios4_2ArraysMultidimensionales/Ejercicio3.java
@@ -3,8 +3,17 @@ package ejercicios4_2ArraysMultidimensionales;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Realiza un procedimiento que reciba una matriz cuadrada. 
+		 *El procedimiento debe buscar las filas que sean exactamente 
+		 *iguales a columnas y mostrar el contenido indicando los índices.
+		 */
+		
+		new Ejercicio3().show();
 
+	}
+	
+	public void show() {
+		
 	}
 
 }
