74cb23b09eb0e6f237be50a32839d6e8752d29b7
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-25 09:33:04 +0100

    Ejercicio 5 empezado

--- src/ejercicios4_2ArraysMultidimensionales/Ejercicio5.java
+++ src/ejercicios4_2ArraysMultidimensionales/Ejercicio5.java
@@ -3,8 +3,18 @@ package ejercicios4_2ArraysMultidimensionales;
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Realiza una función que devuelva si una matriz cuadrada es un 
+		 *cuadrado mágico. Un cuadrado mágico es una matriz cuadrada NxN 
+		 *donde las sumas de cada columna, de cada fila y de cada 
+		 *diagonal son iguales. 
+		 */
+		
+		new Ejercicio5().show();
 
+	}
+	
+	public void show() {
+		
 	}
 
 }
