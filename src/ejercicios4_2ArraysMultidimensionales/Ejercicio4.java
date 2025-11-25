42b1aa976333e1fee85c534dcb6713bfa90bea13
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-25 09:32:06 +0100

    Ejercicio 4 empezado

--- src/ejercicios4_2ArraysMultidimensionales/Ejercicio4.java
+++ src/ejercicios4_2ArraysMultidimensionales/Ejercicio4.java
@@ -3,8 +3,16 @@ package ejercicios4_2ArraysMultidimensionales;
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Realiza una función que reciba una matriz cuadrada y 
+		 *devuelva la suma de cada fila y de cada columna.
+		 */
+		
+		new Ejercicio4().show();
 
+	}
+	
+	public void show() {
+		
 	}
 
 }
