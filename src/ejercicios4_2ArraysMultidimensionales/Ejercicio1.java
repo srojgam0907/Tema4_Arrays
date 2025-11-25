0905cef5c211a29d1b1e74a21d5f6a71b85f39d8
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-25 09:29:08 +0100

    Ejercicio 1 empezado

--- src/ejercicios4_2ArraysMultidimensionales/Ejercicio1.java
+++ src/ejercicios4_2ArraysMultidimensionales/Ejercicio1.java
@@ -3,8 +3,23 @@ package ejercicios4_2ArraysMultidimensionales;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Un array bidimensional llamado matriz y de dimensiones NxN 
+		 *(N filas y N columnas) se dice que es simétrico si sus elementos 
+		 *satisfacen la siguiente condición:
+		 *
+		 *matriz [ i ] [ j ] = matriz [ j ] [ i ] para todo i, j, es decir, 
+		 *si se obtiene el mismo array al cambiar las filas por las columnas. 
+		 *
+		 *Realiza una función que reciba un array bidimensional y devuelva un 
+		 *booleano indicando si es simétrico.
+		 */
 
+		new Ejercicio1().show();
+
+	}
+	
+	public void show() {
+		
 	}
 
 }
