55fe30d79e27c05cf6129dd1b977312c7911de49
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-25 09:34:09 +0100

    Ejercicio 6 empezado

--- src/ejercicios4_2ArraysMultidimensionales/Ejercicio6.java
+++ src/ejercicios4_2ArraysMultidimensionales/Ejercicio6.java
@@ -3,8 +3,20 @@ package ejercicios4_2ArraysMultidimensionales;
 public class Ejercicio6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Se trata de colocar ocho reinas sobre un tablero de ajedrez, 
+		 *de tal forma que ninguna amenace (pueda comerse) a otra. 
+		 *Una reina amenaza a otra pieza que esté en la misma columna, 
+		 *fila o cualquiera de las cuatro diagonales. Escribe un 
+		 *programa que determine todas las combinaciones posibles en las 
+		 *que se puedan acomodar las reinas.
+		 */
+		
+		new Ejercicio6().show();
 
+	}
+	
+	public void show() {
+		
 	}
 
 }
