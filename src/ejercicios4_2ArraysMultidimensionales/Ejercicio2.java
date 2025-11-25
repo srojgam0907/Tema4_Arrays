80e91bc1730d7c7c2b1fb6c94090364e2a46791c
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-25 10:36:49 +0100

    Ejercicio 2 funcion show

--- src/ejercicios4_2ArraysMultidimensionales/Ejercicio2.java
+++ src/ejercicios4_2ArraysMultidimensionales/Ejercicio2.java
@@ -12,7 +12,31 @@ public class Ejercicio2 {
 	}
 	
 	public void show() {
+		int numMayor=0;
+		double [][][] matriz= { // matriz [2]capas [2]filas [3]columnas 
+			{ //capa 0
+				{3.14, 7.82, 0.56},
+				{9.01, 4.77, 8.62}
+			},
+				
+			{ //capa 1
+				{6.53, 2.48, 11.09},
+				{5.21, 13.37, 1.44} 
+			}
+		};
 		
+		numMayor= highElement(matriz);
+		
+		System.out.printf("En esa matriz el numero más grande es %d", numMayor);
+		
+	}
+	
+	public int highElement(double[][][] matriz) { 
+		int mayor=0;
+		
+		
+		
+		return mayor;
 	}
 
 }
