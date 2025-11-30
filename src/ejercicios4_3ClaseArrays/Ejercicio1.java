c792eebd2e79264a400fad216e3fd077ebb13f55
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-30 17:37:51 +0100

    ejercicio 1 funcion show sin terminar

--- src/ejercicios4_3ClaseArrays/Ejercicio1.java
+++ src/ejercicios4_3ClaseArrays/Ejercicio1.java
@@ -3,8 +3,18 @@ package ejercicios4_3ClaseArrays;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*Realiza una función que reciba un array bidimensional de enteros y 
+		 * devuelva otro array bidimensional con los elementos copiados pero 
+		 * con el doble de columnas. Los elementos nuevos se rellenan con ceros. 
+		 * Utiliza copyOf de la clase Arrays.
+		 */
+		
+		new Ejercicio1().show();
+		
+	}
+	
+	public void show() {
+		
 	}
 
 }
