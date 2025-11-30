71243b3cf5aa184c43f4268d61ba3e8bff5918d8
srojgam0907 <srojgam0907@g.educaand.es>
2025-11-30 18:20:20 +0100

    Ejercicio1 funcion show terminada

--- src/ejercicios4_3ClaseArrays/Ejercicio1.java
+++ src/ejercicios4_3ClaseArrays/Ejercicio1.java
@@ -1,12 +1,14 @@
 package ejercicios4_3ClaseArrays;
 
+import java.util.Arrays;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		/*Realiza una función que reciba un array bidimensional de enteros y 
-		 * devuelva otro array bidimensional con los elementos copiados pero 
-		 * con el doble de columnas. Los elementos nuevos se rellenan con ceros. 
-		 * Utiliza copyOf de la clase Arrays.
+		 *devuelva otro array bidimensional con los elementos copiados pero 
+		 *con el doble de columnas. Los elementos nuevos se rellenan con ceros. 
+		 *Utiliza copyOf de la clase Arrays.
 		 */
 		
 		new Ejercicio1().show();
@@ -14,7 +16,29 @@ public class Ejercicio1 {
 	}
 	
 	public void show() {
+		int[][] array= {{1, 2, 3}, {4, 5, 6}};
 		
+		System.out.printf("Nuevo array: %s%n", Arrays.toString(duplicateColumn(array)));
+		
+	}
+	
+	public int[][] duplicateColumn(int[][] array) {
+		int[][] array2; 
+		
+		
+		
+		
+		return array2;
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
