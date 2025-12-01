a6ea182ef0d70003da2751c22172aefc18871761
srojgam0907 <srojgam0907@g.educaand.es>
2025-12-01 10:31:00 +0100

    Ejercicio 4 terminado

--- src/ejercicios4_1RetornoArrays/Ejercicio4.java
+++ src/ejercicios4_1RetornoArrays/Ejercicio4.java
@@ -15,11 +15,12 @@ public class Ejercicio4 {
 	
 	public void show() {
 		int[] array= enterValues();
+		int[] array2= new int [array.length];
 		int index= 0;
 		
 		System.out.printf("Array 1: %s%n\n", Arrays.toString(array));
 		
-		System.out.printf("Array invertido: %s%n\n", Arrays.toString(reverse(array, index)));
+		System.out.printf("Array invertido: %s%n\n", Arrays.toString(reverse(array, array2, index)));
 	}
 	
 	public int[] enterValues() {
@@ -51,17 +52,16 @@ public class Ejercicio4 {
 		return array;
 	}
 	
-	public int[] reverse(int[] array, int index) { //funcion recursiva
-		int[] array2= new int [array.length];
+	public int[] reverse(int[] array, int[] array2, int index) { //funcion recursiva
 		
 		if(array.length == index) { //caso base
 			return array2;
-			
 		}
+		
 		//Se copia el elemento
 		System.arraycopy(array, index, array2, array.length-1-index, 1);
 		
-		array2= reverse(array, index+1);
+		reverse(array, array2, index+1); //caso recursivo
 		
 		return array2;
 	}
