fa5b6f4613844f5e42f1a56cc9b2af2d6f7b1caa
srojgam0907 <srojgam0907@g.educaand.es>
2025-12-01 10:37:24 +0100

    Ejercicio 5 funcion enterValues

--- src/ejercicios4_1RetornoArrays/Ejercicio5.java
+++ src/ejercicios4_1RetornoArrays/Ejercicio5.java
@@ -1,5 +1,7 @@
 package ejercicios4_1RetornoArrays;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
@@ -14,7 +16,38 @@ public class Ejercicio5 {
 	}
 	
 	public void show() {
+		int[] array= enterValues();
 		
+		
+	}
+	
+	public int[] enterValues() {
+		@SuppressWarnings("resource")
+		Scanner kb= new Scanner(System.in);
+		int length= 0;
+		int[] array;
+		int value= 0;
+		
+		do {
+			System.out.println("Introduce una longitud, mayor o igual 2");
+			length= kb.nextInt();
+			
+			if(length < 2) {
+				System.out.println("ERROR numero fuera de rango"); 
+			}
+			
+		} while(length < 2); //La longitud del array no puede ser < 2 
+		
+		array= new int [length]; //Inicializa la longitud del array
+		 
+		for(int i=0; i< array.length; i++) { //Rellena los valores del array
+			System.out.printf("Introduce un valor para la posicion %d\n", i+1);
+			value= kb.nextInt();
+			
+			array[i]= value;
+		}
+		
+		return array;
 	}
 
 }
